663f952c005d7a1c2f78f99822aaf9b4f2c7580e
Alina <alina_rodeanu@yahoo.com>
2019-01-15 14:11:23 +0100

    Update2RegistratieValidatie

--- RegistratieValidatie.js
+++ RegistratieValidatie.js
@@ -3,19 +3,21 @@ function formValidation() {
     var uname1 = document.registratie.voornaam;
     var uname2 = document.registratie.familienaam;
     var brp = document.registratie.beroep;
-    var ugebdat = document.registratie.geboortedatum;
+    var ugebdat = document.registratie.geboorteDatum;
     var uleftijd = document.registratie.leeftijd;
     var ugewicht = document.registratie.gewicht;
     var ugrootte = document.registratie.grootte;
     var uhaar = document.registratie.haarkleur;
     var uogen = document.registratie.oogkleur;
     var uemail = document.registratie.email;
+    // console.log(uemail);
     var umsex = document.registratie.msex;
     var ufsex = document.registratie.fsex;
     var ulichaam = document.registratie.lichaamsbouw;
     var uprovincie = document.registratie.provincie;
     var ustadt = document.registratie.stadt;
-    var passid = document.registratie.passid;
+    var upassid = document.registratie.passid;
+    //   console.log(upassid);
     // foto validatie op het einde van deze code
 
 
@@ -39,7 +41,7 @@ function formValidation() {
                                                                     if (provincieSelect(uprovincie)) {
                                                                         if (stadt_validation(ustadt, 7, 15)) {
                                                                             if (allLetter(ustadt)) {
-                                                                                if (passid_validation(passid, 7, 12)) {}
+                                                                                if (passid_validation(upassid, 7, 12)) {}
                                                                             }
                                                                         }
                                                                     }
@@ -117,13 +119,16 @@ function beroep_validation(brp, mx, my) {
 }*/
 
 
-function geboorteDatum_validation(ugebdat) {
+/*function geboorteDatum_validation(ugebdat) {
 
     // First check for the pattern
     var regex_date = /^\d{4}\-\d{1,2}\-\d{1,2}$/;
-
+    console.log(ugebdat);
     if (!regex_date.test(ugebdat)) {
+        // console.log(ugebdat);
         return false;
+
+
     }
 
     // Parse the date parts to integers
@@ -134,6 +139,8 @@ function geboorteDatum_validation(ugebdat) {
 
     // Check the ranges of month and year
     if (year < 1000 || year > 3000 || month == 0 || month > 12) {
+        alert("blalblalala ");
+
         return false;
     }
 
@@ -146,6 +153,45 @@ function geboorteDatum_validation(ugebdat) {
 
     // Check the range of the day
     return day > 0 && day <= monthLength[month - 1];
+}*/
+
+function geboorteDatum_validation(ugebdat) {
+    var lblError = document.getElementById("lblError");
+
+    //Get the date from the TextBox.
+    var dateString = document.getElementById("geboorteDatum").value;
+    var regex = /(((0|1)[0-9]|2[0-9]|3[0-1])\/(0[1-9]|1[0-2])\/((19|20)\d\d))$/;
+    //var regex = /^\d{4}\-\d{1,2}\-\d{1,2}$/;
+
+    //Check whether valid dd/MM/yyyy Date Format.
+    if (regex.test(dateString)) {
+        var parts = dateString.split("/");
+        var dtDOB = new Date(parts[1] + "/" + parts[0] + "/" + parts[2]);
+        var dtCurrent = new Date();
+        lblError.innerHTML = "Eligibility 18 years ONLY."
+        if (dtCurrent.getFullYear() - dtDOB.getFullYear() < 18) {
+            return false;
+        }
+
+        if (dtCurrent.getFullYear() - dtDOB.getFullYear() == 18) {
+
+            //CD: 11/06/2018 and DB: 15/07/2000. Will turned 18 on 15/07/2018.
+            if (dtCurrent.getMonth() < dtDOB.getMonth()) {
+                return false;
+            }
+            if (dtCurrent.getMonth() == dtDOB.getMonth()) {
+                //CD: 11/06/2018 and DB: 15/06/2000. Will turned 18 on 15/06/2018.
+                if (dtCurrent.getDate() < dtDOB.getDate()) {
+                    return false;
+                }
+            }
+        }
+        lblError.innerHTML = "";
+        return true;
+    } else {
+        lblError.innerHTML = "Enter date in dd/MM/yyyy format ONLY."
+        return false;
+    }
 }
 
 
@@ -176,7 +222,7 @@ function allLetter(uname) {
 
 function haarSelect(uhaar) {
     if (uhaar.value == "Default") {
-        alert('Select your country from the list');
+        alert('Select haarKleur from the list');
         uhaar.focus();
         return false;
     } else {
@@ -186,7 +232,7 @@ function haarSelect(uhaar) {
 
 function ogenSelect(uogen) {
     if (uogen.value == "Default") {
-        alert('Select your country from the list');
+        alert('Select ogenKleur from the list');
         uogen.focus();
         return false;
     } else {
@@ -224,82 +270,83 @@ function validateGeslacht(umsex, ufsex) {
         window.location.reload()
         return true;
     }
+}
 
-    function lichaamsBouwSelect(ulichaam) {
-        if (ulichaam.value == "Default") {
-            alert('Select your country from the list');
-            ulichaam.focus();
-            return false;
-        } else {
-            return true;
-        }
-    }
-
-    function provincieSelect(uprovincie) {
-        if (uprovincie.value == "Default") {
-            alert('Select your country from the list');
-            uprovincie.focus();
-            return false;
-        } else {
-            return true;
-        }
-    }
-
-    function stadt_validation(uid, mx, my) {
-        var uid_len = uid.value.length;
-        if (uid_len == 0 || uid_len >= my || uid_len < mx) {
-            alert("Familienaam should not be empty / length be between " + mx + " to " + my);
-            uid.focus();
-            return false;
-        }
+function lichaamsBouwSelect(ulichaam) {
+    if (ulichaam.value == "Default") {
+        alert('Select lichaamsbouw from the list');
+        ulichaam.focus();
+        return false;
+    } else {
         return true;
     }
+}
 
-
-    function passid_validation(passid, mx, my) {
-        var passid_len = passid.value.length;
-        if (passid_len == 0 || passid_len >= my || passid_len < mx) {
-            alert("Password should not be empty / length be between " + mx + " to " + my);
-            passid.focus();
-            return false;
-        }
+function provincieSelect(uprovincie) {
+    if (uprovincie.value == "Default") {
+        alert('Select provincie from the list');
+        uprovincie.focus();
+        return false;
+    } else {
         return true;
     }
+}
 
-    function ValidateFileUpload() {
-        var fuData = document.getElementById('fileChooser');
-        var FileUploadPath = fuData.value;
+function stadt_validation(uid, mx, my) {
+    var uid_len = uid.value.length;
+    if (uid_len == 0 || uid_len >= my || uid_len < mx) {
+        alert("Stadt should not be empty / length be between " + mx + " to " + my);
+        uid.focus();
+        return false;
+    }
+    return true;
+}
 
-        //To check if user uploaded any file
-        if (FileUploadPath == '') {
-            alert("Please upload an image");
 
-        } else {
-            var Extension = FileUploadPath.substring(
-                FileUploadPath.lastIndexOf('.') + 1).toLowerCase();
+function passid_validation(upassid, mx, my) {
+    var upassid_len = upassid.value.length;
+    if (upassid_len == 0 || upassid_len >= my || upassid_len < mx) {
+        alert("Password should not be empty / length be between " + mx + " to " + my);
+        upassid.focus();
+        return false;
+    }
+    return true;
+}
 
-            //The file uploaded is an image
+function ValidateFileUpload() {
+    var fuData = document.getElementById('fileChooser');
+    var FileUploadPath = fuData.value;
 
-            if (Extension == "gif" || Extension == "png" || Extension == "bmp" ||
-                Extension == "jpeg" || Extension == "jpg") {
+    //To check if user uploaded any file
+    if (FileUploadPath == '') {
+        alert("Please upload an image");
 
-                // To Display
-                if (fuData.files && fuData.files[0]) {
-                    var reader = new FileReader();
+    } else {
+        var Extension = FileUploadPath.substring(
+            FileUploadPath.lastIndexOf('.') + 1).toLowerCase();
 
-                    reader.onload = function(e) {
-                        $('#blah').attr('src', e.target.result);
-                    }
+        //The file uploaded is an image
 
-                    reader.readAsDataURL(fuData.files[0]);
+        if (Extension == "gif" || Extension == "png" || Extension == "bmp" ||
+            Extension == "jpeg" || Extension == "jpg") {
+
+            // To Display
+            if (fuData.files && fuData.files[0]) {
+                var reader = new FileReader();
+
+                reader.onload = function(e) {
+                    $('#blah').attr('src', e.target.result);
                 }
 
+                reader.readAsDataURL(fuData.files[0]);
             }
 
-            //The file upload is NOT an image
-            else {
-                alert("Photo only allows file types of GIF, PNG, JPG, JPEG and BMP. ");
+        }
 
-            }
+        //The file upload is NOT an image
+        else {
+            alert("Photo only allows file types of GIF, PNG, JPG, JPEG and BMP. ");
+
         }
     }
+}
