9b9a8ef36e3bfcb37a84361fa3207124fbfa440d
Khanifa <hanifakz@gmail.com>
2019-01-20 10:22:35 +0100

    feavKnop aleen in public.html met icon change property

--- js/favoritten.js
+++ js/favoritten.js
@@ -1,12 +1,39 @@
-/*$('.like-btn').on('click', function() {
-    $(this).toggleClass('is-active');
- });*/
- function myFunction(x) {
-    x.classList.toggle("fa-thumbs-down");
-    console.log(x.classList.toggle("fa-thumbs-up"));
+function favKnop(pFavknop, user, user1) {
+    var knopClass = favKnopchange(user, user1);
+    console.log(knopClass);
+    pFavknop.setAttribute('class', knopClass);
+    pFavknop.setAttribute('onclick', "myFunction(this)");
+    console.log(pFavknop);
+}
+function favKnopchange(user, user1) {
+    if (user[0].favoritten == "[]" || user[0].favoritten == "") {
+        var added = "fa fa-thumbs-up";
+    }
+
+    else {
+
+        let oldFavorite = JSON.parse(user[0].favoritten);
+        console.log(oldFavorite.indexOf(user1[0]._id), user1[0]._id);
+        if (oldFavorite.indexOf(user1[0]._id) > -1) {
+
+            added = "fa fa-thumbs-down";
+
+        }
+        else {
+            added = "fa fa-thumbs-up";
+            console.log(added);
+
+        }
+
+    }
+    return added;
+}
+function myFunction(x) {
     addFavorite(x);
- }
- function addFavorite(x) {
+    x.classList.toggle("fa-thumbs-down")
+    console.log(x.classList.toggle("fa-thumbs-down"));
+}
+function addFavorite(x) {
     scrumlib.addProperty("favoritten", "String", "");
     scrumlib.addProperty("favorGevers", "String", "");
     // scrumlib.addProperty("provincie", "String", "");
@@ -20,19 +47,23 @@
     var ca = document.cookie.split('=');
     var user = scrumlib.getDatasetById(ca[1]);
 
-    var favUser = user1;
+    var favUser = scrumlib.getDatasetById(ipara);
 
     var arrayFavoritten = [favUser[0]._id];
 
     var updateMapfavorite = {};
-    if (x.classList.toggle("fa-thumbs-up")==true){
- //   if (checkboxElem.checked) {
-        if (user[0].favoritten == '') {
+
+
+    if (x.classList.toggle("fa-thumbs-down")) {
+        //  if (x.checked) {
+        console.log('adding');
+        var add = "true";
+        if (user[0].favoritten == "[]" || user[0].favoritten == "") {
             updateMapfavorite.favoritten = JSON.stringify(arrayFavoritten);
         } else {
             let oldFavorite = JSON.parse(user[0].favoritten);
             if (oldFavorite.indexOf(favUser[0]._id) < 0) {
-                updateMapfavorite.favoritten = favUser[0]._id;
+                //  updateMapfavorite.favoritten = favUser[0]._id;
 
                 oldFavorite = oldFavorite.concat(arrayFavoritten);
                 updateMapfavorite.favoritten = JSON.stringify(oldFavorite);
@@ -42,16 +73,16 @@
             }
         }
 
-        // alert("toegevoegd")
-
     }
     else {
-        let oldFavorite = JSON.parse(user[0].favoritten);
-        console.log(oldFavorite, 'before');
-        if (oldFavorite.indexOf(favUser[0]._id) > -1) {
-            oldFavorite = oldFavorite.splice(oldFavorite.indexOf(favUser[0]._id), favUser[0]._id);
-            console.log(oldFavorite, 'after');
-            updateMapfavorite.favoritten = JSON.stringify(oldFavorite);
+
+        let oldFavorite1 = JSON.parse(user[0].favoritten);
+        console.log(oldFavorite1, 'before');
+        if (oldFavorite1.indexOf(favUser[0]._id) > -1) {
+            console.log(favUser[0]._id, oldFavorite1, 'after');
+            oldFavorite1.splice(oldFavorite1.indexOf(favUser[0]._id), 1);
+            console.log(favUser[0]._id, oldFavorite1, 'after');
+            updateMapfavorite.favoritten = JSON.stringify(oldFavorite1);
         }
         //  alert("niet toegevoegd")
     }
@@ -59,23 +90,24 @@
 
     scrumlib.save();
     //    console.log(user[0].favorite, "   ", counter);
-    updateFavGevers(x, favUser, user, ipara);
+    updateFavGevers(add, favUser, user, ipara);
 
 }
-function updateFavGevers(x, favUser, user, ipara) {
+function updateFavGevers(add, favUser, user, ipara) {
 
 
     var arrayFavGevers = [user[0]._id];
     var updateMapfavGever = {};
- //   if (checkboxElem.checked) {
-    if (x.classList.toggle("fa-thumbs-up")==true){
- 
-        if (favUser[0].favorGevers == '') {
+    //   if (checkboxElem.checked) {
+    if (add == "true") {
+        console.log("favgever");
+        if (favUser[0].favorGevers == "[]" || favUser[0].favorGevers == "") {
+            //      if (favUser[0].favorGevers == '') {
             updateMapfavGever.favorGevers = JSON.stringify(arrayFavGevers);
         } else {
             let oldFavGever = JSON.parse(favUser[0].favorGevers);
             if (oldFavGever.indexOf(user[0]._id) < 0) {
-                updateMapfavGever.favorGevers = user[0]._id;
+                // updateMapfavGever.favorGevers = user[0]._id;
                 oldFavGever = oldFavGever.concat(arrayFavGevers);
                 updateMapfavGever.favorGevers = JSON.stringify(oldFavGever);
             } else {
@@ -86,13 +118,13 @@ function updateFavGevers(x, favUser, user, ipara) {
         // alert("toegevoegd")
     }
     else {
-        let oldFavGever = JSON.parse(favUser[0].favorGevers);
-        if (oldFavGever.indexOf(user[0]._id) > -1) {
-            oldFavGever = oldFavGever.splice(oldFavGever.indexOf(user[0]._id), user[0]._id);
+        let oldFavGever1 = JSON.parse(favUser[0].favorGevers);
+        if (oldFavGever1.indexOf(user[0]._id) > -1) {
+            oldFavGever1.splice(oldFavGever1.indexOf(user[0]._id), 1);
 
-            updateMapfavGever.favorGevers = JSON.stringify(oldFavGever);
+            updateMapfavGever.favorGevers = JSON.stringify(oldFavGever1);
         } else {
-            updateMapfavGever.favorGevers = JSON.stringify(oldFavGever);
+            updateMapfavGever.favorGevers = JSON.stringify(oldFavGever1);
 
         }
         //  alert("niet toegevoegd")
